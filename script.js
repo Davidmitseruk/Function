824e17453924c17e467574afb09fa3268218dee1
Davidmitseruk <davidmitseruk@gmail.com>
2024-06-14 23:03:01 +0200

    lol

--- script.js
+++ script.js
@@ -24,17 +24,34 @@
 // sum(0, 800);
 // sum(2000, 800);
 
-const sum = function (amount, balance){
-    if(amount === 0){
-        console.log('Enter sum more then 0');
-        return;
-    }
-    if(amount > balance){
-        console.log('Not enough money');
-        return;
-    }
-    console.log('Purschased!');
+// const sum = function (amount, balance){
+//     if(amount === 0){
+//         console.log('Enter sum more then 0');
+//         return;
+//     }
+//     if(amount > balance){
+//         console.log('Not enough money');
+//         return;
+//    }
+//     console.log('Purschased!');
+// }
+// sum(400, 800);
+// sum(0, 800);
+// sum(2000, 800);
+
+// const b = 20;
+
+// const add = function(){
+//     const a = 5;
+//     return a + b;
+// }
+// console.log(add());
+
+const value = 20;
+const add = function(number){
+    const value = 10;
+    const inner = 5;
+    return number + value + inner;
 }
-sum(400, 800);
-sum(0, 800);
-sum(2000, 800);
+console.log(value);
+console.log(add(25));
