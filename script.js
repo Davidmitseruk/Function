2a8d3bf816ce5e49c2fcec68c37c4d00a2397b3d
Davidmitseruk <davidmitseruk@gmail.com>
2024-12-30 12:32:14 +0100

    ok

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
 
+let users = JSON.parse(localStorage.getItem('users')) || [];
 
-// const count = function(fromCount = 0, toCount = 5, step = 1){
-//     console.log(`fromCount = ${fromCount}, toCount = ${toCount}, step = ${step}`);
-//     for (let i = fromCount; i<= toCount; i += step);{
-//         console.log(i);
-//     }
-// };
-
-// count(1, 5); 
-
-// const sum = function(amount, balance){
-//     if (amount === 0){
-//         console.log('Enter sum more then 0');
-//     }
-//     else if(amount > balance){
-//         console.log('Not enough money');
-//     }
-//     else{
-//         console.log('Purschased!');
-//     }
-// };
-// sum(400, 800);
-// sum(0, 800);
-// sum(2000, 800);
-
-// const sum = function (amount, balance){
-//     if(amount === 0){
-//         console.log('Enter sum more then 0');
-//         return;
-//     }
-//     if(amount > balance){
-//         console.log('Not enough money');
-//         return;
-//    }
-//     console.log('Purschased!');
-// }
-// sum(400, 800);
-// sum(0, 800);
-// sum(2000, 800);
-
-// const b = 20;
-
-// const add = function(){
-//     const a = 5;
-//     return a + b;
-// }
-// console.log(add());
-
-const value = 20;
-const add = function(number){
-    const value = 10;
-    const inner = 5;
-    return number + value + inner;
+function renderTable() {
+    const tableBody = document.getElementById("userTableBody");
+    tableBody.innerHTML= '';
+    users.forEach((user, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML=`
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+        <td>${user.phone}</td>
+        <td>
+            <button onclick="editUser(${index})">Edit</button>
+            <button onclick="deleteUser(${index})">Delete</button>
+         </td>
+        `;
+        tableBody.appendChild(row);
+    })
 }
-console.log(value);
-console.log(add(25));
+
+function addUser(){
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
+    if (firstName && lastName && email && phone){
+        const user = {firstName, lastName, email, phone};
+        users.push(user);
+        localStorage.setItem('users', JSON.stringify(users));
+
+        document.getElementById('firstName').value = '';
+        document.getElementById('lastName').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('phone').value = '';
+    } else {
+        alert("Please fill all the textboxes!")
+    }
+}
+
+function editUser(index) {
+    const user = users[index];
+    document.getElementById('firstName').value = user.firstName;
+    document.getElementById('lastName').value = user.lastName;
+    document.getElementById('email').value = user.email;
+    document.getElementById('phone').value = user.phone;
+
+    deleteUser(index);
+
+}
+
+function deleteUser(index) {
+    users.splice(index, 1)
+    localStorage.setItem('users', JSON.stringify(users));
+    renderTable();
+}
+renderTable();
