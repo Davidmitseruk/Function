48a31d65903ed8b0b550fea6133278c3985ea0d1
Davidmitseruk <davidmitseruk@gmail.com>
2025-01-03 16:43:27 +0100

    olol

---
+++ js10/script.js
@@ -0,0 +1,42 @@
+// console.log("first log");
+//setTimeout(() => {
+//    console.log("second log")
+//}, 3000)
+
+//setTimeout(() => {
+//    console.log("thrid log");
+//}, 4000);
+
+
+//const one = () => {
+//    console.log("Hello!")
+//}
+//const timer = setTimeout(one, 3000);
+//clearTimeout(timer);
+
+//setTimeout(callback, 1000);
+//setTimeout(callback, 4);
+
+//const button = document.querySelector(".btn");
+//const onClick = () => {
+//    setTimeout(() => {
+ //       alert('I love Javascript!');
+ //   }, 2000);
+//}
+//button.addEventListener('click', onClick);
+
+const startBtn = document.querySelector(".js-start");
+const stopBtn = document.querySelector(".js-stop");
+let timer = null;
+
+startBtn.addEventListener("click", () => {
+    timer = setInterval(() => {
+        console.log(`I love JS ${Math.random()}`)
+    }, 1000);
+})
+
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timer);
+    console.log('interval stopped');
+})
