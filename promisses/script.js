85d7ad5921be3cbf440faa1b404e78a59b0fe9bd
Davidmitseruk <davidmitseruk@gmail.com>
2025-02-05 20:24:31 +0100

    lll

--- promisses/script.js
+++ promisses/script.js
@@ -88,20 +88,74 @@
 // .then(result => console.log(`Result: ${result}`))
 // .catch(error => console.log(error));
 
-function checkEven(numbers){
-    return new Promise((resolve, reject) => {
-        if(!Array.isArray(numbers) || numbers.some(num => typeof num !== 'number')){
-            reject('Ne virni danji');
+//function checkEven(numbers){
+//    return new Promise((resolve, reject) => {
+//        if(!Array.isArray(numbers) || numbers.some(num => typeof num !== 'number')){
+//            reject('Ne virni danji');
+//
+//        } else if(numbers.some(num => num % 2 !== 0)){
+//           reject('ne parni chesla')
+//        } else{
+//            resolve('vsi chesla parni')
+//        }
+//    })
+//}
+//const numbers = [2, 4, 6, 8, 12];
 
-        } else if(numbers.some(num => num % 2 !== 0)){
-            reject('ne parni chesla')
-        } else{
-            resolve('vsi chesla parni')
-        }
-    })
-}
-const numbers = [2, 4, 6, 8, 12];
+//checkEven(numbers)
+//.then(message => console.log("good:", message))
+//.catch(error => console.error(error));
 
-checkEven(numbers)
-.then(message => console.log("good:", message))
-.catch(error => console.error(error));
+//const makePromise = (text, delay) => {
+//    return new Promise(resolve => {
+//        setTimeout(() => resolve(text), delay)
+//    })
+//}
+//const promiseOne = makePromise('promise one', 1500);
+//const promiseTwo = makePromise('promise two', 3000);
+
+//Promise.race([promiseOne, promiseTwo]) 
+//.then((value) => {
+//    console.log(value)
+//})
+//.catch((error) => {console.log(error)})
+
+
+
+
+// Any
+//Promise.any([
+//    new Promise((resolve, reject) => setTimeout(() => reject(new Error('Oops!')), 1000)),
+//    new Promise((resolve, reject) => setTimeout(() => resolve('Correct'), 1500)),
+//    new Promise((resolve, reject) => setTimeout(() => reject(new Error('error!')), 2000)),
+
+//]).then(value => console.log(value))
+//.catch(error => {
+//    console.log(error.contructer.name);
+//    console.log(error.errors[0]);
+//    console.log(error.errors[1]);
+//})
+
+// new Promise(resolve => resolve('succes')).then(value => console.log(value));
+// Promise.resolve('succes').then(value => console.log(value))
+
+//const makePromise = guestName => {
+//    if(guestName === '' || guestName === undefined){
+//        return{
+//            success: false,
+//            message: 'Error guest name'
+//        }
+//    }
+//    return{
+//        success: true,
+//        message: `Welcome ${guestName}`
+//    }
+//
+//}
+//const result = makePromise('David');
+
+//if(result.success){
+//    console.log(result.message)
+//} else{
+//   console.error(result.message)
+//}
